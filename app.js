10d56bdbb84e9873419e89f4c6c807c03baf8cec
Gburis <gabrielleiteolimpio@gmail.com>
2019-08-08 17:41:19 -0300

    63. Introdução a Websocket e Socket.IO

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var app = require('./config/server');
 
-app.listen(3000, function(){
+var server = app.listen(3000, function(){
     console.log('servidor ON');
-})
+});
+
+require('socket.io').listen(server);
